c15712e5eeda2409ece7e8804eb45f164271ec91
Arunof2002 <arunrengasamy9943@gmail.com>
2025-01-30 13:26:46 +0530

    added textbox and css designs

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -7,20 +7,26 @@ function LoginForm() {
     <div className='wrapper bg-dark d-flex align-items-center justify-content-center w-100'>
     <div className='login'>
       <h2 className='mb-3'>Login Form</h2>
-      <form>
-      <div className='form-group mb-2'>
-        <label htmlFor='email'>Email address</label>
-        <input type='email'></input>
+      <form className='needs-validation'>
+      <div className='form-group was-validated mb-2'>
+        <label htmlFor='email' className='form-label'>Email address</label>
+        <input type='email' className='form-control' required></input>
+      <div className='invalid-feedback'>
+       Please enter your email
       </div>
-      <div className='form-group mb-2'>
-        <label htmlFor='password'>Password</label>
-        <input type='password'></input>
       </div>
-      <div className='form-group mb-2'>
-        <label htmlFor='check'>Remember me</label>
-        <input type='checkbox'></input>
+      <div className='form-group was-validated mb-2'>
+        <label htmlFor='password' className='form-label'>Password</label>
+        <input type='password' className='form-control' required></input>
+        <div className='invalid-feedback'>
+       Please enter your Password
       </div>
-      <button type='submit' className='btn btn-success mt-2'>Sign in</button>
+      </div>
+      <div className='form-group form-check mb-2'>
+        <label htmlFor='check' className='form-check-label'>Remember me</label>
+        <input type='checkbox' className='form-check-input'></input>
+      </div>
+      <button type='submit' className='btn btn-success w-100 block mt-2'>Sign in</button>
       </form>
     </div>
     </div>
