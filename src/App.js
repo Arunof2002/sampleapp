745826d1753f1101c15951a58e63e8b7551d1297
Arunof2002 <arunrengasamy9943@gmail.com>
2025-02-03 14:30:53 +0530

    added new desgns and some tags

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import Signup from "./Signup";
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/login">Login</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<LoginForm/>} />
+        <Route path="/Signup" element={<Signup />} />
+      </Routes>
+    </Router> 
+    
+  );
+}
+
+export default App;
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,32 +1,34 @@
 import React from 'react'
 import './LoginForm.css'
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import { FaUser, FaLock  } from "react-icons/fa";
 function LoginForm() {
   return (
     <div className='wrapper bg-dark d-flex align-items-center justify-content-center w-100'>
     <div className='login'>
       <h2 className='mb-3'>Login Form</h2>
       <form className='needs-validation'>
-      <div className='form-group was-validated mb-2'>
+      <div className=' mb-2'>
         <label htmlFor='email' className='form-label'>Email address</label>
-        <input type='email' className='form-control' required></input>
-      <div className='invalid-feedback'>
-       Please enter your email
+        <input type='email' className='form-control' placeholder='Username' required/>
+        <FaUser className='icon'/>
       </div>
-      </div>
-      <div className='form-group was-validated mb-2'>
+      <div className=' mb-2'>
         <label htmlFor='password' className='form-label'>Password</label>
-        <input type='password' className='form-control' required></input>
-        <div className='invalid-feedback'>
-       Please enter your Password
+        <input type='password' className='form-control' placeholder='Password'  required></input>
+        <FaLock className='icon'/>
       </div>
+      <div className=' mb-4'>
+        <label><input type="checkbox"/>Remember me</label>
+        <a href='#'>Forgot password?</a>
+        </div>
+      <button type='submit'>Sign in</button>
+
+      <div className="re-1">
+        <p>Don't have anaccount? <a href='#'>Register</a></p>
       </div>
-      <div className='form-group form-check mb-2'>
-        <label htmlFor='check' className='form-check-label'>Remember me</label>
-        <input type='checkbox' className='form-check-input'></input>
-      </div>
-      <button type='submit' className='btn btn-success w-100 block mt-2'>Sign in</button>
+  
       </form>
     </div>
     </div>
